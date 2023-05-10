d3c95de8001adedaa5b49a312610ecd318033a27
Thomas Lautaro López <laautarolopez@gmail.com>
2023-05-10 12:45:03 -0300

    terminado

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -1,54 +1,88 @@
 import { pool } from "../db.js";
 
 export const getEmpleados = async (req, res) => {
-    const [rows] = await pool.query("SELECT * FROM empleado");
+    try {
+        const [rows] = await pool.query("SELECT * FROM empleado");
+        res.json(rows);
+    } catch(error) {
+        return res.status(500).json({
+            message: "Algo salió mal"
+        });
+    }
 
-    res.json(rows);
 };
 
 export const getEmpleado = async (req, res) => {
     const {id} = req.params;
-    const [rows] = await pool.query('SELECT * FROM empleado WHERE id = ?', [id]);
 
-    if(rows.length <= 0) return res.status(404).json({
-        message: 'Empleado no encontrado'
-    });
+    try {
+        const [rows] = await pool.query('SELECT * FROM empleado WHERE id = ?', [id]);
 
-    res.json(rows[0]);
+        if(rows.length <= 0) return res.status(404).json({
+            message: 'Empleado no encontrado'
+        });
+
+        res.json(rows[0]);
+    } catch(error) {
+        return res.status(500).json({
+            message: "Algo salió mal"
+        });
+    }
 }
 
 export const createEmpleado = async (req, res) => {
     const { nombre, salario } = req.body;
-    const [rows] = await pool.query("INSERT INTO empleado (nombre, salario) VALUES (?, ?)", [nombre, salario])
 
-    res.send({
-        id: rows.insertId,
-        nombre,
-        salario
-    });
+    try {
+        const [rows] = await pool.query("INSERT INTO empleado (nombre, salario) VALUES (?, ?)", [nombre, salario])
+
+        res.send({
+            id: rows.insertId,
+            nombre,
+            salario
+        });
+    } catch(error) {
+        return res.status(500).json({
+            message: "Algo salió mal"
+        });
+    }
 };
 
 export const updateEmpleado = async (req, res) => {
     const {id} = req.params;
     const {nombre, salario} = req.body;
-    const [result] = await pool.query('UPDATE empleado SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id = ?', [nombre, salario, id]);
 
-    if(result.affectedRows <= 0) return res.status(404).json({
-        message: 'Empleado no encontrado'
-    });
+    try {
+        const [result] = await pool.query('UPDATE empleado SET nombre = IFNULL(?, nombre), salario = IFNULL(?, salario) WHERE id = ?', [nombre, salario, id]);
 
-    const [rows] = await pool.query('SELECT * FROM empleado WHERE id = ?', [id]);
+        if(result.affectedRows <= 0) return res.status(404).json({
+            message: 'Empleado no encontrado'
+        });
 
-    res.json(rows[0]);
+        const [rows] = await pool.query('SELECT * FROM empleado WHERE id = ?', [id]);
+
+        res.json(rows[0]);
+    } catch(error) {
+        return res.status(500).json({
+            message: "Algo salió mal"
+        });
+    }
 };
 
 export const deleteEmpleado = async (req, res) => {
     const {id} = req.params;
-    const [rows] = await pool.query('DELETE FROM empleado WHERE id = ?', [id]);
+    
+    try {
+        const [rows] = await pool.query('DELETE FROM empleado WHERE id = ?', [id]);
 
-    if(rows.affectedRows <= 0) return res.status(404).json({
-        message: 'Empleado no encontrado'
-    });
+        if(rows.affectedRows <= 0) return res.status(404).json({
+            message: 'Empleado no encontrado'
+        });
 
-    res.send("El empleado con id " + id + " fue borrado exitosamente.");
+        res.send("El empleado con id " + id + " fue borrado exitosamente.");
+    } catch(error) {
+        return res.status(500).json({
+            message: "Algo salió mal"
+        });
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import express from 'express';
-import empleadosRoutes from './routes/empleados.routes.js';
-import indexRoutes from './routes/index.routes.js';
+import { PORT } from './config.js';
+import app from './app.js';
 
-const app = express();
-
-app.use(express.json());
-
-app.use('/api', indexRoutes);
-app.use('/api', empleadosRoutes);
-
-app.listen(3000);
+app.listen(PORT);
